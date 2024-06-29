6a35d50972e3d8300789a8c942e5fcc2dece0c7a
Shivam Mandal <109310628+Shivam-Mandal@users.noreply.github.com>
2024-06-29 08:10:24 +0530

    Update mailController.js

--- Backend/controllers/mailController.js
+++ Backend/controllers/mailController.js
@@ -11,7 +11,7 @@ const sendmail = async (req, res) => {
             secure: false,
             auth: {
                 user: "shivammandal83404@gmail.com",
-                pass: "vnyo euaq seey lccw",
+                pass: "email password",
             },
         });
 
